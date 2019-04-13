f157afae0240fa227185d98fc5574390eeed2445
Yudai915 <40430090+Yudai915@users.noreply.github.com>
2019-04-13 17:09:42 +0900

    fix AI

--- Pacman/Pacman/PlayerAI.cpp
+++ Pacman/Pacman/PlayerAI.cpp
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include "Info.h"
 
-#define U 0
-#define D 1
-#define L 2
-#define R 3
-#define NO 4
+#define U 10
+#define D 11
+#define L 12
+#define R 13
+#define NO 14
+#define ROAD 0
+#define FOOD 1
+#define WALL 2
 
 bool Enemy_pos(int dir, POSITION p, POSITION e) {
 	switch (dir)
@@ -54,6 +57,31 @@ bool Enemy_pos(int dir, POSITION p, POSITION e) {
 	default: exit(-1);
 		break;
 	}
+}
+
+MOVEMENT Food_Search(MAPDATA map, POSITION p) {
+	if (map[p.y - 1][p.x] == FOOD)
+	{
+		return UP;
+	}
+	if (map[p.y + 1][p.x] == FOOD)
+	{
+		return DOWN;
+	}
+	if (map[p.y][p.x + 1] == FOOD)
+	{
+		return RIGHT;
+	}
+	if (map[p.y][p.x - 1] == FOOD)
+	{
+		return LEFT;
+	}
+
+	return STAY;
+}
+
+MOVEMENT Road_Search(MAPDATA map, POSITION p) {
+
 }
 
 MOVEMENT playerAI(MAPDATA map, POSITION p, POSITION e, int turn)
