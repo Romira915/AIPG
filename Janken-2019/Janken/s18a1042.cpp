55c8f7166ad639c4120039d8dfb5f3d416338663
Yudai915 <40430090+Yudai915@users.noreply.github.com>
2019-04-14 04:23:06 +0900

    履歴から確率化して実装

--- Janken-2019/Janken/s18a1042.cpp
+++ Janken-2019/Janken/s18a1042.cpp
@@ -3,19 +3,80 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#include <cmath>
+
+double softmax(Te target, const double* te_prob) {
+	double te_denial[3];
+	double sum = 0, max = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		te_denial[i] = 100 - te_prob[i];
+		sum += te_denial[i];
+		if (te_denial[i] >= max)
+		{
+			max = te_denial[i];
+		}
+	}
+
+	return (exp(te_denial[target] - max)) / (exp(sum - max));
+}
+
+double* Te_probability(int count, Te rivalhistory[]) {
+	double te_prob[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < count; j++)
+		{
+			if (rivalhistory[j] == i)
+			{
+				te_prob[i]++;
+			}
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		te_prob[i] = te_prob[i] / count;
+	}
+
+	return te_prob;
+}
+
 // Te は Gu=0, Choki=1, Pa=2 という値を持つ enum 型
 // 第一引数 i は１試合の中で現在何回目を示す．0からMAXKAISU-1までの値．
 // 第二引数 myhistory は自分の手の履歴を示す．添字は0からMAXKAISU-1まで使えるが，0からi-1までの履歴しか信用出来ない．
 // 第三引数 rivalhistory は相手の手の履歴を示す．添字は0からMAXKAISU-1まで使えるが，0からi-1までの履歴しか信用出来ない．
 Te s18a1042(int i, Te myhistory[], Te rivalhistory[]) {
-	switch (i) {
-	case 0:
+	if (i == 0)
+	{
+		return Te(rand() % 3);
+	}
+
+	double *te_prob = Te_probability(i, rivalhistory);
+	double next_Te[3];
+	int next_Te_int[3];
+	for (int i = 0; i < 3; i++)
+	{
+		next_Te[i] = softmax(Te(i), te_prob);
+		next_Te_int[i] = int(next_Te[i] * 10000);
+	}
+
+	int te = rand() % 10000 + 1;
+	if (1 <= te && te < next_Te_int[0])
+	{
 		return Gu;
-	case 1:
+	}
+	else if (next_Te_int[0] <= te && te < next_Te_int[0] + next_Te_int[1])
+	{
 		return Choki;
-	case 2:
+	}
+	else if (next_Te_int[0] + next_Te_int[1] <= te && te < next_Te_int[0] + next_Te_int[1] + next_Te_int[3])
+	{
 		return Pa;
-	default:
-		return (Te)((rivalhistory[i-1]+2)%3);
+	}
+	else
+	{
+		return Te(rand() % 3);
 	}
 }
